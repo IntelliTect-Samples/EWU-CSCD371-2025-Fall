1fed4f9716183303c48c1b43e551fabedf96bf33
steeley21 <ksteele5@ewu.edu>
2025-09-29 14:30:52 -0700

    Fixed questions array logic

--- PrincessBrideTrivia/PrincessBrideTrivia/Program.cs
+++ PrincessBrideTrivia/PrincessBrideTrivia/Program.cs
@@ -86,7 +86,12 @@ public class Program
             question.Answers[1] = answer2;
             question.Answers[2] = answer3;
             question.CorrectAnswerIndex = correctAnswerIndex;
+
+            questions[i] = question;   
         }
         return questions;
     }
+
+    
+
 }

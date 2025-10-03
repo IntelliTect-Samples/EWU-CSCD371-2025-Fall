58a1cd3d5a73fb3b3097b578066bb8ea5cc62aff
ColtonKnopik <ColtonKnopik@gmail.com>
2025-10-03 14:30:36 -0700

    LogFactory Tests

--- Logger.Tests/LogFactoryTests.cs
+++ Logger.Tests/LogFactoryTests.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+using System;
+using System.IO;
+
 namespace Logger.Tests;
 
 [TestClass]
@@ -13,9 +16,50 @@ public class LogFactoryTests
         var factory = new LogFactory();
 
         //Act
-        var logger = factory.CreateLogger(nameof(factory));
+        var logger = factory.CreateLogger(nameof(LogFactoryTests));
 
         //Assert
         Assert.IsNull(logger);
+    }
+
+    [TestMethod]
+    public void LogFactory_CreateLoggerReturnsLogger_Success()
+    {
+        //Arrange
+        var factory = new LogFactory();
+        string path = "goodpath";
+        factory.ConfigureFileLogger(path);
+        //Act
+        var logger = factory.CreateLogger(nameof(LogFactoryTests));
+        //Assert
+        Assert.IsNotNull(logger);
+        Assert.IsInstanceOfType(logger, typeof(FileLogger));
+        Assert.AreEqual((logger as FileLogger)?.GetFilePath(), path);
+    }
+
+    [TestMethod]
+    public void LogFactory_ConfigureFileLoggerWithInvalidPath_ThrowsException()
+    {
+        //Arrange
+        var factory = new LogFactory();
+        //Act & Assert
+        Assert.ThrowsException<ArgumentException>(() => factory.ConfigureFileLogger(null));
+        Assert.ThrowsException<ArgumentException>(() => factory.ConfigureFileLogger(""));
+        Assert.ThrowsException<ArgumentException>(() => factory.ConfigureFileLogger("   "));
+    }
+
+    [TestMethod]
+    public void LogFactory_ConfigureFileLoggerWithValidPath_Success()
+    {
+        //Arrange
+        var factory = new LogFactory();
+        string path = "goodpath";
+        //Act
+        factory.ConfigureFileLogger(path);
+        var logger = factory.CreateLogger(nameof(LogFactoryTests));
+        //Assert
+        Assert.IsNotNull(logger);
+        Assert.IsInstanceOfType(logger, typeof(FileLogger));
+        Assert.AreEqual((logger as FileLogger)?.GetFilePath(), path);
     }
 }

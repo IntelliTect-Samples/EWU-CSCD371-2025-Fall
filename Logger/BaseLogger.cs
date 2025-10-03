ead7d32ebc7ef02e6972c14c631df871c65c9b0f
ColtonKnopik <ColtonKnopik@gmail.com>
2025-10-03 14:51:12 -0700

    Adds default class name for baselogger

--- Logger/BaseLogger.cs
+++ Logger/BaseLogger.cs
@@ -2,8 +2,14 @@
 
 public abstract class BaseLogger
 {
-    
     public string ClassName { get; set; }
+
+    protected BaseLogger()
+    {
+        // Default to the actual derived class name
+        ClassName = GetType().Name;
+    }
+
     public abstract void Log(LogLevel logLevel, string message);
 }
 
